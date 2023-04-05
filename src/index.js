478d3d83f91dc7f06e678368c4ac442eac179447
omar bida <lordbida55@gmail.com>
2023-04-05 11:39:17 +0100

    setup express app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const dotenv = require("dotenv");
+const express = require("express");
 dotenv.config();
 const db = require("./db");
-
+const routes = require("./routes");
 async function main() {
   await db.connect();
+
+  const app = express();
+  app.use(express.json());
+  routes(app);
+
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
 }
 main();
