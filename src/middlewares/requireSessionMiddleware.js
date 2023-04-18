2ab11a8dd22eebab38dadfa47506042645025c0c
omar bida <lordbida55@gmail.com>
2023-04-18 12:54:45 +0100

    implement require session

---
+++ src/middlewares/requireSessionMiddleware.js
@@ -0,0 +1,22 @@
+const { SessionModel } = require("../db");
+module.exports = {
+  requireSession: async (req, res, next) => {
+    const sessionToken = req.headers["x-session-token"];
+    const session = await SessionModel.findOne({
+      token: sessionToken,
+    });
+    if (!session) {
+      return res.json({
+        message: "Invalid Session Token",
+      });
+    }
+    const hasExpired = session.hasExpired();
+    if (hasExpired) {
+      return res.json({
+        message: "Session Token Expired",
+      });
+    }
+    res.locals.session = session;
+    next();
+  },
+};
--- src/routes/NoteRoutes.js
+++ src/routes/NoteRoutes.js
@@ -1,7 +1,9 @@
 const { NoteModel } = require("../db");
+const { requireSession } = require("../middlewares/requireSessionMiddleware");
 
 module.exports = (app) => {
-  app.post("/notes", async (req, res, next) => {
+  app.post("/notes", requireSession, async (req, res, next) => {
+    const session = res.locals.session;
     const { title, content } = req.body;
     if (!title || !content) {
       return res.status(400).json({
@@ -16,22 +18,30 @@ module.exports = (app) => {
     const note = await NoteModel.create({
       title: title.trim(),
       content: content.trim(),
+      user: session.user,
     });
     return res.status(201).json({
       message: "Note created",
       data: note,
     });
   });
-  app.get("/notes", async (req, res, next) => {
-    const notes = await NoteModel.find({});
+  app.get("/notes", requireSession, async (req, res, next) => {
+    const session = res.locals.session;
+    const notes = await NoteModel.find({
+      user: session.user,
+    });
     return res.status(200).json({
       message: "Notes retrieved",
       data: notes,
     });
   });
-  app.get("/notes/:id", async (req, res, next) => {
+  app.get("/notes/:id", requireSession, async (req, res, next) => {
+    const session = res.locals.session;
     const { id } = req.params;
-    const note = await NoteModel.findById(id);
+    const note = await NoteModel.findOne({
+      _id: id,
+      user: session.user,
+    });
     if (!note) {
       return res.status(404).json({
         message: "Note not found",
@@ -42,7 +52,8 @@ module.exports = (app) => {
       data: note,
     });
   });
-  app.put("/notes/:id", async (req, res, next) => {
+  app.put("/notes/:id", requireSession, async (req, res, next) => {
+    const session = res.locals.session;
     const { id } = req.params;
     const { title, content } = req.body;
     if (!title && !content) {
@@ -56,7 +67,10 @@ module.exports = (app) => {
         message: "title is too long",
       });
     }
-    const note = await NoteModel.findById(id);
+    const note = await NoteModel.findOne({
+      _id: id,
+      user: session.user,
+    });
     if (!note) {
       return res.status(404).json({
         message: "Note not found",
@@ -75,11 +89,13 @@ module.exports = (app) => {
       data: note,
     });
   });
-  app.delete("/notes/:id", async (req, res, next) => {
+  app.delete("/notes/:id", requireSession, async (req, res, next) => {
+    const session = res.locals.session;
     const { id } = req.params;
-    const note = await NoteModel.findByIdAndDelete(id).catch((err) =>
-      console.log(err.message)
-    );
+    const note = await NoteModel.findOneAndDelete({
+      _id: id,
+      user: session.user,
+    }).catch((err) => console.log(err.message));
     if (!note) {
       return res.status(404).json({
         message: "Note not found",
