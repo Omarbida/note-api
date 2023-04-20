eedac0804352de40236a9e9eacbef51819a5f7d3
omar bida <lordbida55@gmail.com>
2023-04-20 09:36:27 +0100

    installed cors

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const express = require("express");
 dotenv.config();
 const db = require("./db");
 const routes = require("./routes");
+const cors = require("cors");
 async function main() {
   await db.connect();
 
   const app = express();
   app.use(express.json());
+  app.use(cors());
   routes(app);
 
   app.listen(process.env.PORT, () => {
